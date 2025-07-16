793093af263d477c325bae54db63d5fcc17a6456
Matias Vargas <matias.vargas.work@gmail.com>
2025-07-16 00:33:25 -0300

    change to neogit

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,5 +1,9 @@
 
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+vim.keymap.set("n", "<leader>gs", vim.cmd.Neogit)
 
--- require("neogit").setup({})
--- require("gitsigns").setup({})
+require("neogit").setup({})
+require("gitsigns").setup({})
+
+vim.keymap.set("v", "<leader>hs", function()
+    vim.cmd("Gitsigns stage_hunk")
+end, { desc = "Neogit stage hunk" })
